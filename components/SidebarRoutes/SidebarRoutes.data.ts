18d5da5f85f6abecf8534c34787899845541825d
Ariel Narvaez <janarvaez11@espe.edu.ec>
2025-02-11 01:25:24 -0500

    add table AMEF

--- components/SidebarRoutes/SidebarRoutes.data.ts
+++ components/SidebarRoutes/SidebarRoutes.data.ts
@@ -43,7 +43,7 @@ export const dataToolsSidebar = [
     {
         icon: CircleHelpIcon,
         label: "AMEF",
-        href: "/faqs"
+        href: "/AMEF"
     },
     {
         icon: BarChart4,
